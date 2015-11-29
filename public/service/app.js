61a3ec2fcf785b82d0ce0ad6553fd53c3cda11f1
Nevio Vesic <nevio.vesic@gmail.com>
2015-11-29 21:49:25 +0100

    Adding ngprogress

--- public/service/app.js
+++ public/service/app.js
@@ -1,5 +1,14 @@
-angular.module('golanghr.slackinvite', ['ui.bootstrap']);
+angular.module('golanghr.slackinvite', ['ui.bootstrap', 'ngProgress']);
 
-angular.module('golanghr.slackinvite').controller('IndexCtrl', function ($rootScope, $scope) {
+angular.module('golanghr.slackinvite').controller('IndexCtrl', function ($rootScope, $scope, ngProgressFactory) {
+  $scope.progressbar = ngProgressFactory.createInstance();
+  $scope.progressbar.setColor('#02bbff');
 
+  $scope.$on('$routeChangeStart', function(next, current) {
+    $scope.progressbar.start();
+  });
+
+  $scope.$on('$routeChangeSuccess', function () {
+		$scope.progressbar.complete();
+	});
 });
